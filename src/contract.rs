1c079d4b06d3ef1961875ef3bce30553b32e3e77
kbhat1 <kartikbhatri@gmail.com>
2023-05-09 15:42:30 -0400

    Add panic to sudo order placement

--- src/contract.rs
+++ src/contract.rs
@@ -171,6 +171,7 @@ fn process_bulk_order_placements(
     orders: Vec<OrderPlacement>,
     deposits: Vec<DepositInfo>,
 ) -> Result<Response, ContractError> {
+    panic!("process_bulk_order_placements");
     let mut unsuccessful_orders = vec![];
     for deposit in deposits {
         let mut balance = get_balance(
